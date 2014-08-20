5a84ee5d529de6d44c176e80cfb50dedc3d83d6e
gcanti <giulio.canti@gmail.com>
2014-08-20 17:31:49 +0200

    playground-alpha

--- playground/playground.js
+++ playground/playground.js
@@ -45,10 +45,19 @@ var TabPane = bs.TabPane;
 var Tooltip = bs.Tooltip;
 var Well = bs.Well;
 
+var isDebuggerEnabled = false;
+
 // override default fail behaviour of tcomb
 t.options.onFail = function (message) {
+  if (isDebuggerEnabled) {
+    debugger;
+  }
   throw new Error(message);
 };
+
+//
+// utils
+//
 
 function getErrorAlert(message){
   return (
@@ -58,27 +67,91 @@ function getErrorAlert(message){
   );
 }
 
+var examples = {
+  Accordion: '<Accordion>\n  <Panel header="Collapsible Group Item #1" key={1}> Anim pariatur cliche reprehenderit </Panel>\n  <Panel header="Collapsible Group Item #2" key={2}> Anim pariatur cliche reprehenderit</Panel>\n  <Panel header="Collapsible Group Item #3" key={3}>Anim pariatur cliche reprehenderit\n</Panel>\n</Accordion>',
+  Alert: '<Alert bsStyle="warning">\n  <strong>Holy guacamole!</strong>\n</Alert>',
+  Badge: '<p>Badges <Badge>42</Badge></p>',
+  Button: '<Button bsStyle="danger" bsSize="medium">Save</Button>',
+  ButtonGroup: '<ButtonGroup>\n  <Button>Left</Button>\n  <Button>Middle</Button>\n  <Button>Right</Button>\n</ButtonGroup>',
+  ButtonToolbar: '<ButtonToolbar>\n  <Button>Default</Button>\n  <Button bsStyle="primary">Primary</Button>\n  <Button bsStyle="success">Success</Button>\n  <Button bsStyle="info">Info</Button>\n  <Button bsStyle="warning">Warning</Button>\n  <Button bsStyle="danger">Danger</Button>\n  <Button bsStyle="link">Link</Button>\n</ButtonToolbar>',
+  Carousel: '<Carousel>\n  <CarouselItem>\n  <img width={900} height={500} alt="900x500" src="carousel.png"/>\n  <div className="carousel-caption">\n  <h3>First slide label</h3> <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>\n  </div>\n  </CarouselItem>\n  <CarouselItem>\n  <img width={900} height={500} alt="900x500" src="carousel.png"/>\n  <div className="carousel-caption">\n  <h3>Second slide label</h3>\n  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n  </div>\n  </CarouselItem>\n  <CarouselItem>\n  <img width={900} height={500} alt="900x500" src="carousel.png"/>\n  <div className="carousel-caption">\n  <h3>Third slide label</h3>\n  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>\n  </div>\n  </CarouselItem>\n  </Carousel>',
+  DropdownButton: '<DropdownButton bsStyle={title.toLowerCase()} title={title}> <MenuItem key="1">Action</MenuItem> <MenuItem key="2">Another action</MenuItem> <MenuItem key="3">Something else here</MenuItem> <MenuItem divider /> <MenuItem key="4">Separated link</MenuItem> </DropdownButton>',
+  Glyphicon: '<div> <ButtonToolbar> <ButtonGroup> <Button><Glyphicon glyph="align-left" /></Button> <Button><Glyphicon glyph="align-center" /></Button> <Button><Glyphicon glyph="align-right" /></Button> <Button><Glyphicon glyph="align-justify" /></Button> </ButtonGroup> </ButtonToolbar> <ButtonToolbar> <ButtonGroup> <Button bsSize="large"><Glyphicon glyph="star" /> Star</Button> <Button><Glyphicon glyph="star" /> Star</Button> <Button bsSize="small"><Glyphicon glyph="star" /> Star</Button> <Button bsSize="xsmall"><Glyphicon glyph="star" /> Star</Button> </ButtonGroup> </ButtonToolbar> </div>',
+  Input: '<form> <Input type="text" defaultValue="text" /> <Input type="password" defaultValue="secret" /> <Input type="checkbox" checked readOnly label="checkbox"/> <Input type="radio" checked readOnly label="radio"/> <Input type="select" defaultValue="select"> <option value="select">select</option> <option value="other">...</option> </Input> <Input type="select" multiple> <option value="select">select (multiple)</option> <option value="other">...</option> </Input> <Input type="textarea" defaultValue="textarea" /> <Input type="static" value="static" /> </form>',
+  Modal: '<Modal title="Modal title" backdrop={false} animation={false} onRequestHide={handleHide}> <div className="modal-body"> One fine body... </div> <div className="modal-footer"> <Button>Close</Button> <Button bsStyle="primary">Save changes</Button> </div> </Modal>',
+  Nav: '<Nav bsStyle="pills" activeKey={1}>\n  <NavItem key={1} href="/home">NavItem 1 content</NavItem>\n  <NavItem key={2} title="Item">NavItem 2 content</NavItem>\n  <NavItem key={3} disabled={true}>NavItem 3 content</NavItem>\n</Nav>',
+  Navbar: '<Navbar>\n  <Nav>\n  <NavItem key={1} href="#">Link</NavItem>\n  <NavItem key={2} href="#">Link</NavItem>\n  <DropdownButton key={3} title="Dropdown">\n  <MenuItem key="1">Action</MenuItem>\n  <MenuItem key="2">Another action</MenuItem>\n  <MenuItem key="3">Something else here</MenuItem>\n  <MenuItem divider />\n  <MenuItem key="4">Separated link</MenuItem>\n  </DropdownButton>\n  </Nav>\n</Navbar>',
+  Pager: '<Pager>\n  <PageItem href="#">Previous</PageItem>\n  <PageItem href="#">Next</PageItem>\n</Pager>',
+  PanelGroup: '<PanelGroup activeKey={1} onSelect={handlePanelGroupSelect.bind(this)} accordion>\n  <Panel header="Panel 1" key={1}>Panel 1 content</Panel>\n  <Panel header="Panel 2" key={2}>Panel 2 content</Panel>\n</PanelGroup>',
+  Popover: '<div>\n  <Popover placement="right" positionLeft={200} positionTop={50} title="Popover right"> And here\'s some <strong>amazing</strong> content. It\'s very engaging. right? </Popover>\n</div>',
+  ProgressBar: '<div>\n  <ProgressBar striped bsStyle="success" now={40} />\n  <ProgressBar striped bsStyle="info" now={20} />\n  <ProgressBar striped bsStyle="warning" now={60} />\n  <ProgressBar striped bsStyle="danger" now={80} />\n</div>',
+  SplitButton: '<SplitButton bsStyle="success" title="success">\n  <MenuItem key="1">Action</MenuItem>\n  <MenuItem key="2">Another action</MenuItem>\n  <MenuItem key="3">Something else here</MenuItem>\n  <MenuItem divider />\n  <MenuItem key="4">Separated link</MenuItem>\n</SplitButton>',
+  TabbedArea: '<TabbedArea defaultActiveKey={2}>\n  <TabPane key={1} tab="Tab 1"><p>TabPane 1 content</p></TabPane>\n  <TabPane key={2} tab="Tab 2"><p>TabPane 2 content</p></TabPane>\n</TabbedArea>',
+  Table: '<Table striped bordered condensed hover>\n  <thead>\n  <tr>\n  <th>#</th>\n  <th>First Name</th>\n  <th>Last Name</th>\n  <th>Username</th>\n  </tr>\n  </thead>\n  <tbody>\n  <tr>\n  <td>1</td>\n  <td>Mark</td>\n  <td>Otto</td>\n  <td>@mdo</td>\n  </tr>\n  <tr>\n  <td>2</td>\n  <td>Jacob</td>\n  <td>Thornton</td>\n  <td>@fat</td>\n  </tr>\n  <tr>\n  <td>3</td>\n  <td colSpan="2">Larry the Bird</td>\n  <td>@twitter</td>\n  </tr>\n  </tbody>\n  </Table>',
+  Tooltip: '<div>\n  <Tooltip placement="right" positionLeft={150} positionTop={50}><strong>Holy guacamole!</strong> Check this info. </Tooltip>\n</div>',
+  Well: '<div>\n  <Well bsSize="large">Look I\'m in a large well!</Well>\n  <Well bsSize="small">Look I\'m in a small well!</Well>\n</div>'        
+};
+
+var options = Object.keys(examples).sort().map(function (k) {
+  return React.DOM.option({key: k, value: k}, k);
+});
+
+var JSX_PREAMBLE = '/** @jsx React.DOM */\n';
+
+function handleHide() {
+  $('.modal').remove();
+}
+
+function handlePanelGroupSelect(selectedKey) {
+}
+
+//
+// components
+//
+
+function projectLink(title) {
+  return React.DOM.a({href: "https://github.com/gcanti/tcomb-react-bootstrap"}, title)
+}
+
 var Header = React.createClass({displayName: 'Header',
   render: function () {
     return (
-      React.DOM.h1(null, "Playground")
+      Row(null, 
+        Col({md: 6}, 
+          React.DOM.h1(null, "Playground")
+        ), 
+        Col({md: 6}
+        )
+      )
+    );
+  }
+});
+
+var Footer = React.createClass({displayName: 'Footer',
+  render: function () {
+    return (
+      Row(null, 
+        Col({md: 12}, 
+          React.DOM.p({className: "text-muted"}, "Built with ", projectLink('tcomb-react-bootstrap'))
+        )
+      )
     );
   }
 });
 
 var Code = React.createClass({displayName: 'Code',
-  onChange: function () {
-    var value = this.refs.code.getValue();
-    this.props.onChange(value);
-  },
   render: function () {
     return (
-      Input({
-        ref: "code", 
-        type: "textarea", 
-        value: this.props.value, 
-        rows: "10", 
-        onChange: this.onChange})
+      React.DOM.div(null, 
+        React.DOM.label(null, "Code (Live reload)"), 
+        React.DOM.pre({className: "jsx-preamble"}, JSX_PREAMBLE), 
+        Input({
+          ref: "code", 
+          type: "textarea", 
+          value: this.props.value, 
+          rows: "10", 
+          onChange: this.props.onChange})
+      )
     );
   }
 });
@@ -91,42 +164,10 @@ var Preview = React.createClass({displayName: 'Preview',
   }
 });
 
-var examples = {
-  Accordion: '<Accordion> <Panel header="Collapsible Group Item #1" key={1}> Anim pariatur cliche reprehenderit </Panel> <Panel header="Collapsible Group Item #2" key={2}> Anim pariatur cliche reprehenderit </Panel> <Panel header="Collapsible Group Item #3" key={3}> Anim pariatur cliche reprehenderit </Panel> </Accordion>',
-  Alert: '<Alert bsStyle="warning"><strong>Holy guacamole!</strong></Alert>',
-  Badge: '<p>Badges <Badge>42</Badge></p>',
-  Button: '<Button bsStyle="danger" bsSize="large">Save</Button>',
-  ButtonGroup: '<ButtonGroup> <Button>Left</Button> <Button>Middle</Button> <Button>Right</Button> </ButtonGroup>',
-  ButtonToolbar: '<ButtonToolbar> <Button>Default</Button> <Button bsStyle="primary">Primary</Button> <Button bsStyle="success">Success</Button> <Button bsStyle="info">Info</Button> <Button bsStyle="warning">Warning</Button> <Button bsStyle="danger">Danger</Button> <Button bsStyle="link">Link</Button> </ButtonToolbar>',
-  Carousel: '<Carousel> <CarouselItem> <img width={900} height={500} alt="900x500" src="assets/carousel.png"/> <div className="carousel-caption"> <h3>First slide label</h3> <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> </div> </CarouselItem> <CarouselItem> <img width={900} height={500} alt="900x500" src="assets/carousel.png"/> <div className="carousel-caption"> <h3>Second slide label</h3> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> </div> </CarouselItem> <CarouselItem> <img width={900} height={500} alt="900x500" src="assets/carousel.png"/> <div className="carousel-caption"> <h3>Third slide label</h3> <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> </div> </CarouselItem> </Carousel>',
-  DropdownButton: '<DropdownButton bsStyle={title.toLowerCase()} title={title}> <MenuItem key="1">Action</MenuItem> <MenuItem key="2">Another action</MenuItem> <MenuItem key="3">Something else here</MenuItem> <MenuItem divider /> <MenuItem key="4">Separated link</MenuItem> </DropdownButton>',
-  Glyphicon: '<div> <ButtonToolbar> <ButtonGroup> <Button><Glyphicon glyph="align-left" /></Button> <Button><Glyphicon glyph="align-center" /></Button> <Button><Glyphicon glyph="align-right" /></Button> <Button><Glyphicon glyph="align-justify" /></Button> </ButtonGroup> </ButtonToolbar> <ButtonToolbar> <ButtonGroup> <Button bsSize="large"><Glyphicon glyph="star" /> Star</Button> <Button><Glyphicon glyph="star" /> Star</Button> <Button bsSize="small"><Glyphicon glyph="star" /> Star</Button> <Button bsSize="xsmall"><Glyphicon glyph="star" /> Star</Button> </ButtonGroup> </ButtonToolbar> </div>',
-  Input: '<form> <Input type="text" defaultValue="text" /> <Input type="password" defaultValue="secret" /> <Input type="checkbox" checked readOnly label="checkbox"/> <Input type="radio" checked readOnly label="radio"/> <Input type="select" defaultValue="select"> <option value="select">select</option> <option value="other">...</option> </Input> <Input type="select" multiple> <option value="select">select (multiple)</option> <option value="other">...</option> </Input> <Input type="textarea" defaultValue="textarea" /> <Input type="static" value="static" /> </form>',
-  Modal: '<Modal title="Modal title" backdrop={false} animation={false} onRequestHide={handleHide}> <div className="modal-body"> One fine body... </div> <div className="modal-footer"> <Button>Close</Button> <Button bsStyle="primary">Save changes</Button> </div> </Modal>',
-  Nav: '<Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}> <NavItem key={1} href="/home">NavItem 1 content</NavItem> <NavItem key={2} title="Item">NavItem 2 content</NavItem> <NavItem key={3} disabled={true}>NavItem 3 content</NavItem> </Nav>',
-  Navbar: '<Navbar> <Nav> <NavItem key={1} href="#">Link</NavItem> <NavItem key={2} href="#">Link</NavItem> <DropdownButton key={3} title="Dropdown"> <MenuItem key="1">Action</MenuItem> <MenuItem key="2">Another action</MenuItem> <MenuItem key="3">Something else here</MenuItem> <MenuItem divider /> <MenuItem key="4">Separated link</MenuItem> </DropdownButton> </Nav> </Navbar>',
-  Pager: '<Pager> <PageItem href="#">Previous</PageItem> <PageItem href="#">Next</PageItem> </Pager>',
-  PanelGroup: ' <PanelGroup activeKey={1} onSelect={handleSelect} accordion> <Panel header="Panel 1" key={1}>Panel 1 content</Panel> <Panel header="Panel 2" key={2}>Panel 2 content</Panel> </PanelGroup>',
-  Popover: '<div style={holderStyle}> <Popover placement="right" positionLeft={200} positionTop={50} title="Popover right"> And here\'s some <strong>amazing</strong> content. It\'s very engaging. right? </Popover> </div>',
-  ProgressBar: '<div> <ProgressBar striped bsStyle="success" now={40} /> <ProgressBar striped bsStyle="info" now={20} /> <ProgressBar striped bsStyle="warning" now={60} /> <ProgressBar striped bsStyle="danger" now={80} /> </div>',
-  SplitButton: '<SplitButton bsStyle={title.toLowerCase()} title={title}> <MenuItem key="1">Action</MenuItem> <MenuItem key="2">Another action</MenuItem> <MenuItem key="3">Something else here</MenuItem> <MenuItem divider /> <MenuItem key="4">Separated link</MenuItem> </SplitButton>',
-  TabbedArea: '<TabbedArea defaultActiveKey={2}> <TabPane key={1} tab="Tab 1">TabPane 1 content</TabPane> <TabPane key={2} tab="Tab 2">TabPane 2 content</TabPane> </TabbedArea>',
-  Table: '<Table striped bordered condensed hover> <thead> <tr> <th>#</th> <th>First Name</th> <th>Last Name</th> <th>Username</th> </tr> </thead> <tbody> <tr> <td>1</td> <td>Mark</td> <td>Otto</td> <td>@mdo</td> </tr> <tr> <td>2</td> <td>Jacob</td> <td>Thornton</td> <td>@fat</td> </tr> <tr> <td>3</td> <td colSpan="2">Larry the Bird</td> <td>@twitter</td> </tr> </tbody> </Table>',
-  Tooltip: '<div style={holderStyle}> <Tooltip placement="right" positionLeft={150} positionTop={50}> <strong>Holy guacamole!</strong> Check this info. </Tooltip> </div>',
-  Well: '<div> <Well bsSize="large">Look I\'m in a large well!</Well> <Well bsSize="small">Look I\'m in a small well!</Well> </div>'        
-};
-var options = Object.keys(examples).sort().map(function (k) {
-  return React.DOM.option({key: k, value: k}, k);
-});
-
 var Example = React.createClass({displayName: 'Example',
-  onChange: function () {
-    var value = this.refs.example.getValue();
-    this.props.onChange(value);
-  },
   render: function () {
     return (
-      Input({ref: "example", type: "select", defaultValue: "Alert", onChange: this.onChange}, 
+      Input({label: "Examples", ref: "example", type: "select", defaultValue: "Alert", onChange: this.props.onChange}, 
         options
       )
     );
@@ -136,7 +177,7 @@ var Example = React.createClass({displayName: 'Example',
 var Main = React.createClass({displayName: 'Main',
   getInitialState: function () {
     return {
-      code: '/** @jsx React.DOM */\n<Alert bsStyle="warning"><strong>Holy guacamole!</strong></Alert>'
+      code: examples.Alert
     };
   },
   eval: function (code) {
@@ -147,36 +188,47 @@ var Main = React.createClass({displayName: 'Main',
       return getErrorAlert(e.message);
     }
   },
-  onExampleChange: function () {
-
-  },
-  onCodeChange: function (code) {
+  onExampleChange: function (evt) {
+    var value = evt.target.value;
+    var code = examples[value];
     this.setState({code: code});
+  },
+  onCodeChange: function (evt) {
+    var code = evt.target.value;
+    this.setState({code: code});
+  },
+  onDebuggerChange: function (evt) {
+    isDebuggerEnabled = evt.target.checked;
   },
   render: function () {
     var code = this.state.code;
-    var component = this.eval(code);
+    var component = this.eval(JSX_PREAMBLE + code);
+    var debuggerLabel = React.DOM.p(null, React.DOM.strong({className: "text-danger"}, "Enable debugger"), " (remember to open up the console and enjoy)");
     return (
       Grid(null, 
-        Row(null, 
-          Col({md: 12}, 
-            Header(null)
-          )
-        ), 
+        Header(null), 
+        React.DOM.hr(null), 
         Row(null, 
           Col({md: 6}, 
             React.DOM.p({className: "lead"}, "Insert JSX code or choose an example"), 
-            Example({onChange: onExampleChange}), 
-            Code({value: code, onChange: this.onCodeChange})
+            Example({onChange: this.onExampleChange}), 
+            Code({value: code, onChange: this.onCodeChange}), 
+            Input({type: "checkbox", label: debuggerLabel, onChange: this.onDebuggerChange})
           ), 
           Col({md: 6}, 
             React.DOM.p({className: "lead"}, "Preview"), 
             Preview({component: component})
           )
-        )
+        ), 
+        React.DOM.hr(null), 
+        Footer(null)
       )
     );
   }
 });
 
-React.renderComponent(Main(null), document.getElementById('app'));
+//
+// run
+//
+var main = Main(null);
+React.renderComponent(main, document.getElementById('app'));
