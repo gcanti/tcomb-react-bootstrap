c50a0568ff768b3051e9c9dac2f7b6bcc38b5dff
gcanti <giulio.canti@gmail.com>
2014-08-22 10:16:06 +0200

    [REFACTORING] v0.0.3
    allow require a single component

---
+++ test/test.js
@@ -0,0 +1,24 @@
+"use strict";
+var assert = require('assert');
+
+//
+// setup
+//
+
+var ok = function (x) { assert.strictEqual(true, x); };
+var ko = function (x) { assert.strictEqual(false, x); };
+var eq = assert.strictEqual;
+var throwsWithMessage = function (f, message) {
+    assert['throws'](f, function (err) {
+        ok(err instanceof Error);
+        eq(err.message, message);
+        return true;
+    });
+};
+var doesNotThrow = assert.doesNotThrow;
+
+describe('components', function () {
+  it('should being required singly', function () {
+    ok(typeof require('../Alert') === 'function');
+  });
+});
