1337c2f2e5e97dbfb527f021b74a17e96c24e570
gcanti <giulio.canti@gmail.com>
2014-08-23 09:57:29 +0200

    [ADDED] children prop
    Only Buttons are allowed as children

--- ButtonGroup.js
+++ ButtonGroup.js
@@ -2,8 +2,10 @@ var t = require('tcomb');
 var model = require('./util/model');
 var BootstrapMixin = require('./util/BootstrapMixin');
 var Component = require('react-bootstrap/ButtonGroup');
+var Button = require('./Button');
 
 var Model = model.create('ButtonGroup', {
+  children:  t.list(Button.Model),
   vertical:  t.maybe(t.Bool),
   justified: t.maybe(t.Bool)
 }, [BootstrapMixin]);
